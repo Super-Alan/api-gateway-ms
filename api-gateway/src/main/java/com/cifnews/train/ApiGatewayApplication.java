676cb2d7a36bce924d24ae0e71352ecb54254a1e
root <zhao.dingyi@foxmail.com>
2018-05-28 07:51:39 +0800

    add gateway

--- api-gateway/src/main/java/com/cifnews/train/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/cifnews/train/ApiGatewayApplication.java
@@ -2,11 +2,13 @@ package com.cifnews.train;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
