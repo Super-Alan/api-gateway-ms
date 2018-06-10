fe839e0fd41bfb4ccae8e9130547dba686b06f28
root <zhao.dingyi@foxmail.com>
2018-06-10 22:23:28 +0800

    microservice call  by feign with oauth

--- order-service/src/main/java/com/cifnews/train/OrderServiceApplication.java
+++ order-service/src/main/java/com/cifnews/train/OrderServiceApplication.java
@@ -1,20 +1,48 @@
 package com.cifnews.train;
 
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 
 
 @EnableEurekaClient
 @EnableFeignClients
 @EnableAutoConfiguration
 @EnableDiscoveryClient
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
 public class OrderServiceApplication {
+	//Feign 访问内部服务时添加 鉴权头
+	@Bean
+	public RequestInterceptor requestTokenBearerInterceptor() {
 
+		return new RequestInterceptor() {
+
+			@Override
+			public void apply(RequestTemplate requestTemplate) {
+
+				OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails)
+
+						SecurityContextHolder.getContext().getAuthentication()
+								.getDetails();
+
+				requestTemplate.header("Authorization",
+						"bearer " + details.getTokenValue());
+
+			}
+
+		};
+
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
--- order-service/src/main/java/com/cifnews/train/controller/OrderController.java
+++ order-service/src/main/java/com/cifnews/train/controller/OrderController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,6 +39,7 @@ public class OrderController {
      * @return
      */
     @PostMapping("/orders")
+    @PreAuthorize("hasAnyAuthority('query')")
     public SettlementParamDto createOrder(HttpServletRequest request,
                                       @RequestBody @Validated SettlementParamDto settlementParamDto){
        SettlementParamDto paramDto = orderService.createOrder(settlementParamDto);
